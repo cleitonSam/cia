5d2c22f09da133d4d83d613c5eb710275806dda3
cleiotnSam <cleiton.nso2019@gmail.com>
2025-07-15 19:27:11 -0300

    ajuste

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,16 +1,13 @@
 import { Routes } from '@angular/router';
-import { LoginComponent } from './components/login/login.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
-import { authGuard } from './guards/auth.guard';
 import { LayoutComponent } from './components/layout/layout.component';
 
 
 export const routes: Routes = [
-  { path: 'login', component: LoginComponent },
   {
     path: '',
     component: LayoutComponent,
-    canActivate: [authGuard], // Usa o novo guard funcional
+    canActivate: [], // Usa o novo guard funcional
     children: [
       { path: 'dashboard', component: DashboardComponent },
       { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
